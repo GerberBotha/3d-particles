9036d066e5786087a92ab818711483c5ed47674d
GerberBotha <47314815+GerberBotha@users.noreply.github.com>
2021-05-26 13:39:18 +0200

    master

---
+++ src/model.js
@@ -0,0 +1,33 @@
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+
+class Model {
+    constructor (obj) {
+        //console.log(obj)
+        this.name = obj.name
+        this.file = obj.file
+        this.scene = obj.scene
+
+        this.loader = new GLTFLoader()
+        this.dracoLoader = new DRACOLoader()
+        this.dracoLoader.setDecoderPath('./draco/')
+        this.loader.setDRACOLoader(this.dracoLoader)
+
+        this.init()
+    }
+
+    init() {
+        this.loader.load(this.file, (response) => {
+            console.log(response)
+
+            this.mesh = response.scene.children[0]
+            this.material = new THREE.MeshBasicMaterial({
+                color: 'red'
+            })
+            this.mesh.material = this.material
+            this.scene.add(this.mesh)
+        })
+    }
+}
+export default Model
